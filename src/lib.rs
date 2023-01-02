f968d1b1cf6404a4e6d9e63a9ec93faf33efff1e
Patrick Hlustik <patrick.hlustik@gmail.com>
2023-01-02 23:48:52 +0100

    implement the ability to capture values in the json body

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use hyper_tls::HttpsConnector;
 use strum_macros::EnumIter;
 use strum::IntoEnumIterator;
 use colored::*;
+use bytes::BufMut;
 
 mod utils;
 
@@ -33,6 +34,8 @@ struct Endpoint {
     method: String,
     status: u16,
     json_body: Option<HashMap<String, String>>,
+    time_boundaries: Option<[u128; 3]>, // (green), yellow, red, timeout
+    capture: Option<HashMap<String, String>>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -40,8 +43,8 @@ struct Config {
     api_address: String,
     verbose: Option<bool>,
     tests: Vec<Endpoint>,
-    bearer_token: Option<String>,
     time_boundaries: Option<[u128; 3]>, // (green), yellow, red, timeout
+    caption_path: Option<Vec<String>>,
 }
 
 // Checks if a given method matched one of HttpMethod
@@ -51,7 +54,8 @@ fn validate_http_method(method: &String) -> Option<HttpMethod> {
 }
 
 async fn fetch_url(url: hyper::Uri, method: HttpMethod, verbose: bool, timeout: u128,
-     body: String, response_time: &mut u128 /*OUT*/) -> Result<hyper::Response<hyper::Body>> {
+     body: String, response_time: &mut u128 /*OUT*/, buffer: &mut bytes::BytesMut /*OUT*/)
+     -> Result<hyper::Response<hyper::Body>> {
      
     // TLS implementation to enable https requests
     let https = HttpsConnector::new();
@@ -92,13 +96,17 @@ async fn fetch_url(url: hyper::Uri, method: HttpMethod, verbose: bool, timeout: 
 
     println!("Response Status: {}", response.status());
 
+    // stream body data into buffer
+    while let Some(next) = response.data().await {
+        buffer.put(next?);
+    }
+
     if verbose {
         println!("Response Header: {:#?}", response.headers());
 
-        println!("Response Body: "); 
-        while let Some(next) = response.data().await {
-            let chunk = next?;
-            io::Write::write_all(&mut io::stdout(), &chunk)?;
+        if !buffer.is_empty() {
+            println!("Response Body: ");
+            io::Write::write_all(&mut io::stdout(), buffer)?;
         }
     };
 
@@ -121,38 +129,44 @@ pub async fn execute_tests() {
     let verbose = rest_test_config.verbose.unwrap_or(true);
 
     let test_count = rest_test_config.tests.len();
-    let mut method: HttpMethod;
-    let mut route: &String;
     let mut test_index = 0;
     let mut tests_passed = 0;
-    let mut response_time: u128 = 0;
 
     // Get boundaries, set to default values if not found
-    let time_boundaries = rest_test_config.time_boundaries.unwrap_or([500, 1000, 10000]);
+    let mut time_boundaries = rest_test_config.time_boundaries.unwrap_or([500, 1000, 10000]);
 
-    for test in rest_test_config.tests.iter() { 
+    let mut captures: HashMap<String, String> = Default::default();
+
+    for test in rest_test_config.tests.iter() {
+        let mut response_time: u128 = 0;
         test_index += 1;
 
         // Determine criticalness, default to false
         let is_critical = test.critical.unwrap_or(false);
+
+        // Overwrite time boundaries if there is a local definition
+        time_boundaries = match test.time_boundaries {
+            Some(value) => value,
+            None => time_boundaries,
+        };
 
         // Print current test index
         println!("Test {}/{}", test_index, test_count);
 
         // Print test description if available
         match &test.it { 
-            Some(description) => println!("it {}", description),
+            Some(description) => println!("{}", description),
             None => (),
         };
 
         // Check if the http method is valid
-        method = match validate_http_method(&test.method) {
+        let method = match validate_http_method(&test.method) {
             Some(value) => value,
             None => panic!("Unknown or unsupported method {}", test.method),
         };
 
         // Construct the api url
-        route = &test.route;
+        let route = &test.route;
         let url = api_address.to_owned() + route;
         let url = url.parse::<hyper::Uri>().unwrap();
 
@@ -170,9 +184,12 @@ pub async fn execute_tests() {
         }
         body += "}";
 
+        // Create buffer for the response body
+        let mut buffer = bytes::BytesMut::with_capacity(512);
+
         // Send the request and get the response
         let response = match fetch_url(url, method,
-             verbose, time_boundaries[2], body, &mut response_time).await {
+             verbose, time_boundaries[2], body, &mut response_time, &mut buffer).await {
             Ok(res) => res,
             Err(error) => { 
                 println!("Error while sending request: {}", error);
@@ -183,6 +200,34 @@ pub async fn execute_tests() {
                 continue
             },
         };
+
+        let response_status = response.status();
+
+        if !buffer.is_empty() {
+        let json_body: serde_json::Value = match serde_json::from_str(&String::from_utf8_lossy(&buffer)) {
+                Ok(value) => value,
+                Err(error) => {
+                    println!("Error while parsing response body as json: {}\n", error);
+                    continue;
+                },
+            };
+
+        
+            // Capture desired values from the response body
+            match &test.capture {
+                Some(capture) => {
+                    for (key, value) in capture.iter() {
+                        let captured_value = &json_body["data"][value];
+                        if !captured_value.is_null() {
+                            captures.insert(key.to_string(), json_body["data"][value].to_string());
+                        } else {
+                            println!("Cannot capture nonexistent value '{}'", value.bold());
+                        }
+                    }
+                },
+                None => (),
+            }
+        }
 
         let response_time_output = format!("Response time: {} ms", response_time);
 
@@ -195,10 +240,10 @@ pub async fn execute_tests() {
         }
  
         // Check expectations
-        println!("Expected Status: {}", response.status());
+        println!("Expected Status: {}", test.status);
 
         // Print outcome
-        if response.status() == test.status {
+        if response_status == test.status {
             tests_passed += 1;
             println!("{}", "TEST PASSED\n".green().bold())
         } else {
