91ae073f3c12c5e8217df75d52cd69b7060e0656
Patrick Hlustik <patrick.hlustik@googlemail.com>
2023-03-25 11:46:28 +0100

    fix build via npm i

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use rust_rest_test::{ get_config_file, execute_tests };
+
 #[tokio::main]
 async fn main() {
-    let test_file = rrtlib::get_config_file();
+    let test_file = get_config_file();
 
-    rrtlib::execute_tests(test_file).await;
+    execute_tests(test_file).await;
 }
